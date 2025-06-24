28c4b9d100eae5a95b2951877c418f57b7337e4a
Petra-78 <partos.petra.0305@gmail.com>
2025-06-24 17:37:34 +0200

    add HTML and CSS UI and connect with JS

--- script.js
+++ script.js
@@ -20,15 +20,14 @@ const Gameboard = (function() {
     }
 
     const newBoard = () => makeBoard();
+    const getBoard = () => board;
 
-    const getBoard = function() {
-        return board;
-    }
     return {getBoard, newBoard}   
 })();
 
 
 const Player = (function() {
+
     const players = [
         {name: "player1", marker: "x"}, 
         {name: "player2", marker: "o"}
@@ -51,6 +50,7 @@ const Player = (function() {
 
 
 const Game = (function() {
+
     function gameRound(row, col) {
         const marker = Player.getActivePlayer().marker
         if (row > 2 || col > 2 || row < 0 || col < 0) {
@@ -59,17 +59,21 @@ const Game = (function() {
             if (Gameboard.getBoard()[row][col] === 0) {
                 Gameboard.getBoard()[row][col] = marker;
                 console.log(Gameboard.getBoard());
-                checkWinner();
-                Player.switchActivePlayer();
+
+                if (checkWinner()) {
+                    renderDOM.cleanBoard();
+                    restartGame();
+                    
+                } else Player.switchActivePlayer(); 
+
             } else {
                 alert("that spot is already taken!")
             } 
         } 
     } 
 
-    const playRound = (row, col) => gameRound(row, col);
-
     const checkWinner = function() {
+
         function allEqual(a, b, c) {
             return a === b && b === c && a !== 0;
         }
@@ -79,21 +83,66 @@ const Game = (function() {
             if ((allEqual(board[i][0], board[i][1], board[i][2]) ||
                 allEqual (board[0][0], board[1][1], board[2][2]) ||
                 allEqual(board[0][2], board[1][1], board[2][0]))) {
+
                 console.log(`${Player.getActivePlayer().name} won`)
+                return true
             }
             for (j = 0; j < 3; j++) {
                 if (allEqual(board[0][j], board[1][j], board[2][j])) {
                     console.log(`${Player.getActivePlayer().name} won`)
+                    return true
                 } 
             }
-        }
+        } 
+        return false
     }
+
+    const restartGame = function() {
+        const restart = Gameboard.newBoard();
+        return restart
+    }
+
+    const playRound = (row, col) => gameRound(row, col);
+
     return {playRound} 
 })();
 
 Player.switchActivePlayer();
+Gameboard.newBoard();
 
-const restartGame = function() {
-    const restart = Gameboard.newBoard();
-    return restart
-}
+const renderDOM = (function() {
+    const cells = document.querySelectorAll(".cell");
+
+    function changeCell() {
+        const columns = 3;
+
+        cells.forEach((cell, index) => {
+            const row = Math.floor(index / columns);
+            const col = index % columns;
+
+            cell.dataset.row = row;
+            cell.dataset.col = col;
+
+            cell.addEventListener ("click", () => {
+                const row = parseInt(cell.dataset.row);
+                const col = parseInt(cell.dataset.col);
+
+                Game.playRound(row, col);
+                const currentValue = Gameboard.getBoard()[row][col];
+                cell.textContent = currentValue;
+            })
+        })
+    }
+
+    function cleanBoard() {
+        cells.forEach((cell) => {
+            cell.textContent = ""
+        })
+    }
+
+    const getChangeCell = () => changeCell();
+    return {getChangeCell, cleanBoard};
+})();
+
+renderDOM.getChangeCell();
+
